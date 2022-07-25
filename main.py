d52cffa479e150a11e6ee44b0397e89ed51a8c17
AnaAn01 <anastasia.anto01@gmail.com>
2022-07-25 21:30:36 +0300

    full interface

--- main.py
+++ main.py
@@ -26,8 +26,16 @@ canvas = Canvas(width=200, height=224, bg=YELLOW, highlightthickness=0)
 tomato_image = PhotoImage(file='tomato.png')
 canvas.create_image(100, 112, image=tomato_image)
 canvas.create_text(100, 138, text='00:00', fill='white', font=(FONT_NAME, 35, 'bold'))
-canvas.pack()
+canvas.grid(row=1, column=1)
 
+label_title = Label(text="Timer", bg=YELLOW, fg=GREEN, font=(FONT_NAME, 25, 'bold'))
+label_title.grid(row=0, column=1)
+
+but_start = Button(text="Start", font=(FONT_NAME, 15, 'bold'))
+but_start.grid(row=2, column=0)
+
+but_reset = Button(text="Reset", font=(FONT_NAME, 15, 'bold'))
+but_reset.grid(row=2, column=2)
 
 window.mainloop()
 
